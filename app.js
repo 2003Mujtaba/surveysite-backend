593e38a8541cffce608cd4ad64b702d85b2f981f
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-11-19 19:10:46 -0500

    Adjusted routes in app.js

--- app.js
+++ app.js
@@ -10,18 +10,22 @@ const methodOverride = require('method-override');
 
 // Initializes the Express application
 const app = express();
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
 
 // Connects to MongoDB
-mongoose.connect(process.env.DB_URI)
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => {
   console.log('MongoDB connected successfully.');
 
+  // Start the server only after the database connection is established
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
@@ -31,72 +35,18 @@ mongoose.connect(process.env.DB_URI)
   process.exit(1); // Exits the process if MongoDB connection fails
 });
 
-// Imports the model
-const Assignment = require('./models/Assignment');
+// Import the survey routes
+const surveyRoutes = require('/routes/surveyRoutes');
 
-// Define a route for the home page
+// Use survey routes
+app.use('/surveys', surveyRoutes);
+
+// Define a route for the home page to redirect to the surveys page
 app.get('/', (req, res) => {
-  res.render('home'); // This will render the home.ejs file
+  res.redirect('/surveys'); // Redirects to the survey listing page
 });
 
-// Route to display all assignments
-app.get('/assignments', async (req, res) => {
-  try {
-    const assignments = await Assignment.find({});
-    res.render('assignments', { assignments });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error retrieving assignments from database');
-  }
-});
-// Route to display the form for a new assignment
-app.get('/assignments/new', (req, res) => {
-    res.render('new-assignment'); // This EJS file will contain the form
-  });
+// No longer need to import or use the Assignment model and routes
+// Remove any code related to the Assignment model and routes
 
-// Route to handle the form submission for creating a new assignment
-app.post('/assignments', async (req, res) => {
-    try {
-      const newAssignment = new Assignment(req.body);
-      await newAssignment.save();
-      res.redirect('/assignments'); // Redirects to the list of all assignments
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error creating a new assignment'); // Error catch
-    }
-  });
-// Route to display the form to edit an existing assignment
-app.get('/assignments/:id/edit', async (req, res) => {
-    try {
-      const assignment = await Assignment.findById(req.params.id);
-      res.render('edit-assignment', { assignment }); // Passes the assignment to the EJS file
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error loading the assignment to edit');
-    }
-  });
-    
-// Route to handle the form submission for updating an existing assignment
-app.put('/assignments/:id', async (req, res) => {
-    try {
-      await Assignment.findByIdAndUpdate(req.params.id, req.body);
-      res.redirect('/assignments');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error updating the assignment');
-    }
-  });
-  
-// Route to handle the deletion of an assignment
-app.delete('/assignments/:id', async (req, res) => {
-    try {
-      await Assignment.findByIdAndDelete(req.params.id);
-      res.redirect('/assignments');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error deleting the assignment');
-    }
-  });
-  
-// PORT
-const PORT = process.env.PORT || 3000;
+// If there are additional models and routes you will be using, import and use them here similarly to surveyRoutes
