57cb2c5dd49d3227e8443e1a31463a36b8e5f1a6
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-11-20 18:25:16 -0500

    dd

--- routes/surveycontroller.js
+++ routes/surveycontroller.js
@@ -36,8 +36,7 @@ exports.surveyCreatePost = async (req, res) => {
     await survey.save();
     res.redirect('/surveys');
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send(error.message);
   }
 };
 
