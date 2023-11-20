21d04547bd9bb0037f65be742ba48527bae93284
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-11-19 22:49:55 -0500

    d

--- routes/surveyroutes.js
+++ routes/surveyroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const surveyController = require('./controllers/surveycontroller');
+const surveyController = require('./surveycontroller');
 
 // Route to display all surveys
 router.get('/', surveyController.listSurveys);
