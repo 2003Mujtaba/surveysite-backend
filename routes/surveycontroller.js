80634c1c11a0a7928947ebc697de91cac7cad661
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-11-20 23:44:47 -0500

    ss

--- routes/surveycontroller.js
+++ routes/surveycontroller.js
@@ -34,7 +34,7 @@ exports.surveyCreatePost = async (req, res) => {
       questions: req.body.questions,               // Make sure to structure the questions input correctly on the client-side
     });
     await survey.save();
-    res.redirect('/home');
+    res.redirect('/surveys');
   } catch (error) {
     res.status(500).send(error.message);
   }
