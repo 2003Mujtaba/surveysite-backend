ab9bccf2c4c1335e0b039030536ceaf67599e3e5
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-11-20 23:21:31 -0500

    de

--- models/survey.js
+++ models/survey.js
@@ -36,6 +36,6 @@ const surveySchema = new mongoose.Schema({
   }
 });
 
-const Survey = mongoose.model('Survey', surveySchema);
+const Survey = mongoose.model('survey', surveySchema);
 
 module.exports = Survey;
