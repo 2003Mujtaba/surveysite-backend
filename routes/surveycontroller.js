918a69971a92748618d7f43ac24d9b3872067b82
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-12-09 15:18:49 -0500

    Initial commit

--- routes/surveycontroller.js
+++ routes/surveycontroller.js
@@ -1,10 +1,11 @@
-const Survey = require('../models/survey'); // Import the Survey model
+const Survey = require("../models/survey"); // Import the Survey model
 
 // Display list of all surveys
 exports.listSurveys = async (req, res) => {
   try {
     const surveys = await Survey.find();
-    res.render('surveys/list-survey', { surveys });
+    res.status(200).send(surveys);
+    //res.render("surveys/list-survey", { surveys });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -14,7 +15,7 @@ exports.listSurveys = async (req, res) => {
 exports.surveyDetail = async (req, res) => {
   try {
     const survey = await Survey.findById(req.params.id);
-    res.render('surveys/survey-detail', { survey });
+    res.status(200).send(survey);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -22,7 +23,7 @@ exports.surveyDetail = async (req, res) => {
 
 // Display survey create form on GET
 exports.surveyCreateGet = (req, res) => {
-  res.render('surveys/new-survey');
+  res.render("surveys/new-survey");
 };
 
 // Handle survey create on POST
@@ -31,10 +32,10 @@ exports.surveyCreatePost = async (req, res) => {
     const survey = new Survey({
       title: req.body.title,
       description: req.body.description,
-      questions: req.body.questions,               // Make sure to structure the questions input correctly on the client-side
+      questions: req.body.questions, // Make sure to structure the questions input correctly on the client-side
     });
     await survey.save();
-    res.redirect('/surveys');
+    res.status(200).send(survey);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -43,19 +44,19 @@ exports.surveyCreatePost = async (req, res) => {
 // Handle survey delete on DELETE.
 exports.surveyDeleteGet = async (req, res) => {
   try {
-    await Survey.findByIdAndDelete(req.params.id);
-    res.redirect('/surveys');
+    const response = await Survey.findByIdAndDelete(req.params.id);
+    console.log(response);
+    res.status(200).send({message: "success"});
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
-
 // Display survey update form on GET
 exports.surveyUpdateGet = async (req, res) => {
   try {
     const survey = await Survey.findById(req.params.id);
-    res.render('surveys/edit-survey', { survey });
+    res.render("surveys/edit-survey", { survey });
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -65,7 +66,7 @@ exports.surveyUpdateGet = async (req, res) => {
 exports.surveyUpdatePost = async (req, res) => {
   try {
     const survey = await Survey.findByIdAndUpdate(req.params.id, req.body);
-    res.redirect('/surveys');
+    res.status(200).send(survey);
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -78,21 +79,27 @@ exports.surveySubmitPost = async (req, res) => {
     const survey = await Survey.findById(req.params.id);
 
     // Create a new response object based on the submitted answers
-    const newResponse = {
-      responses: req.body.questions.map((question, index) => {
-        return {
-          questionText: survey.questions[index].questionText,
-          response: req.body[`question_${index}`]
-        };
-      })
-    };
+    console.log(req.body);
+    if (Array.isArray(req.body.questions)) {
+      const newResponse = {
+        responses: req.body.questions.map((question, index) => {
+          return {
+            questionText: survey.questions[index].questionText,
+            response: req.body[`question_${index}`],
+          };
+        }),
+      };
+      console.log(newResponse);
 
-    // Update the survey with the new response
-    survey.responses.push(newResponse);
-    await survey.save();
+      // Update the survey with the new response
+      survey.responses.push(newResponse);
+      await survey.save();
+    } else {
+      return res.redirect("/surveys");
+    }
 
     // Redirect or send a success message
-    res.redirect('/surveys/' + req.params.id);
+    res.redirect("/surveys/" + req.params.id);
   } catch (error) {
     res.status(500).send(error.message);
   }
--- routes/surveyroutes.js
+++ routes/surveyroutes.js
@@ -1,30 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const surveyController = require('./surveycontroller');
+const surveyController = require("./surveycontroller");
 
 // Route to display all surveys
-router.get('/', surveyController.listSurveys);
+router.get("/", surveyController.listSurveys);
 
 // Route to display the form for creating a new survey
-router.get('/new', surveyController.surveyCreateGet);
+router.get("/new", surveyController.surveyCreateGet);
 
 // Route to handle the form submission for creating a new survey
-router.post('/', surveyController.surveyCreatePost);
+router.post("/", surveyController.surveyCreatePost);
 
 // Route to display a single survey's detail
-router.get('/:id', surveyController.surveyDetail);
+router.get("/:id", surveyController.surveyDetail);
 
 // Route to display the form to edit an existing survey
-router.get('/:id/edit', surveyController.surveyUpdateGet);
+router.get("/:id/edit", surveyController.surveyUpdateGet);
 
 // Route to handle the form submission for updating an existing survey
-router.put('/:id', surveyController.surveyUpdatePost);
+router.put("/:id", surveyController.surveyUpdatePost);
 
 // Route to handle the deletion of a survey
-router.delete('/:id', surveyController.surveyDeleteGet);
+router.delete("/:id", surveyController.surveyDeleteGet);
 
 // Route to handle the survey submission
-router.post('/:id/submit', surveyController.surveySubmitPost);
-
+router.post("/:id/submit", surveyController.surveySubmitPost);
 
 module.exports = router;
